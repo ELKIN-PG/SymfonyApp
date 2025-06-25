61b625c2721c9d18d46e0b661b346739b080f8c3
ElkinJS <perezgilelkinalbert@gmail.com>
2025-06-24 22:43:32 -0500

    004 - Correcion Bug Route Home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,16 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
     #Establecemos la ruta inicial del proyecto
 
-    /**
-     * @Route("/", name="homepage")
-     */    
+    #[Route('/home', name: 'home')]    
     public function homePage(): Response
     {
         return $this->render('home/homepage.html.twig');
